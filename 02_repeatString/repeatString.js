df606faa254bab2d0bf90e95f5396e3e63268b22
swl23 <139284802+swl23@users.noreply.github.com>
2024-11-12 09:14:26 -0800

    Update function to work properly

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = string;
+    if (num === 0) {
+        result = '';
+        return result
+    }
+    else if (num < 0) {
+        result = 'ERROR';
+        return result
+    }
+    let i = 1;
+    while (i < num) {
+        result += string;
+        i++
+    }
+    return result
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(correctWord) {
+    const reverseWord = [];
+    for (i = correctWord.length - 1; i >= 0; i--) {
+        reverseWord.push(correctWord[i])
+    };
+    let finalWord = '';
+    for (letter of reverseWord) {
+        finalWord += letter;
+    }
+    return finalWord;
 };
 
 // Do not edit below this line
