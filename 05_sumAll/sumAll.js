0b03e0272b07f39dea35fb82823d3fa163154c62
swl23 <139284802+swl23@users.noreply.github.com>
2024-11-13 09:10:10 -0800

    Update code with long version of answer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,37 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0) {
+        return 'ERROR';
+    }
+    if (!Number.isInteger(a)) {
+        return 'ERROR';
+    }
+    if (b < 0) {
+        return 'ERROR';
+    }
+    if (!Number.isInteger(b)) {
+        return 'ERROR';
+    }
+    if (typeof a !== 'number') {
+        return 'ERROR';
+    }
+    if (typeof b !== 'number') {
+        return 'ERROR';
+    }
+    let biggerNumber = 0;
+    let smallerNumber = 0;
+    if (a > b) {
+        biggerNumber = a;
+        smallerNumber = b;
+    }
+    else if (a < b) {
+        biggerNumber = b;
+        smallerNumber = a;
+    }
+    let finalTotal = 0;
+    for (i = 0; i <= (biggerNumber - smallerNumber); i++) {
+        finalTotal += (smallerNumber + i)
+    }
+    return finalTotal
 };
 
 // Do not edit below this line
