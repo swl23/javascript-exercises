c6d193395a8025dca1230292477423ca880bda0d
swl23 <139284802+swl23@users.noreply.github.com>
2024-11-13 19:21:16 -0800

    Update script with functional code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitInput) {
+  celsiusConversion = ((fahrenheitInput - 32) * (5/9));
+  const celsiusAsString = celsiusConversion.toString();
+  if (celsiusAsString.includes('.')) {
+    let finalCelsius = Number(celsiusConversion.toFixed(1));
+    return finalCelsius
+  }
+  else {
+    let finalCelsius = Number(celsiusConversion);
+    return finalCelsius
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusInput) {
+  fahrenheitConversion = (celsiusInput * (9/5) + 32);
+  const fahrenheitAsString = fahrenheitConversion.toString();
+  if (fahrenheitAsString.includes('.')) {
+    let finalFahrenheit = Number(fahrenheitConversion.toFixed(1));
+    return finalFahrenheit
+  }
+  else {
+    let finalFahrenheit = Number(fahrenheitConversion);
+    return finalFahrenheit
+  } 
 };
 
 // Do not edit below this line
