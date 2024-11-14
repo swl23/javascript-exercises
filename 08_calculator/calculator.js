4c09ae5c937024a65996d8110e8e3bf626418c30
swl23 <139284802+swl23@users.noreply.github.com>
2024-11-14 12:57:37 -0800

    Add clunky solutions to calculator problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,56 @@
-const add = function() {
-	
+const add = function(a, b) {
+	result = (a + b);
+  return result
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	result = (a - b);
+  return result
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  for (item of array) {
+    result += item
+  }
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let initialValue = 1;
+  const result = array.reduce(
+    (accumulator, currentValue) => accumulator * currentValue, initialValue);
+  return result
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	const result = a ** b;
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    let result = 1;
+    return result
+  }
+  else if (a === 1) {
+    let result = 1;
+    return result
+  }
+  else {
+    let b = a - 1;
+    let array = [];
+    for (i = 0; i <= b; i++) {
+      array.push(i);
+    }
+    array.shift();
+    console.log(array);
+    const initialValue = 1;
+    const product = array.reduce(
+      (accumulator, currentValue) => accumulator * currentValue, initialValue);
+    const result = (product * a);
+    return result
+  }
 };
 
 // Do not edit below this line
