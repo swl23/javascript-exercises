8cdd49378a43d7886db4c18a499af14949b1be9d
swl23 <139284802+swl23@users.noreply.github.com>
2024-11-14 13:52:20 -0800

    Add sloppy but working functions to script

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const originalString = [];
+    const punctuationList = ['!', '.', ',', ' '];
+    for (i = 0; i < phrase.length; i++) {
+        if (punctuationList.includes(phrase[i])) {
+            continue
+        }
+        else {
+            originalString.push(phrase[i].toLowerCase())
+        }
+    }
+    const reverseString = [];
+    for (j = originalString.length - 1; j >= 0; j--) {
+        reverseString.push(originalString[j])
+    };
+    original = originalString.join('');
+    reverse = reverseString.join('');
+    console.log(original);
+    console.log(reverse);
+    if (original === reverse) {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
