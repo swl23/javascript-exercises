e2fd8c03ee48e7e0a9e4018752f43c1c6e56801c
swl23 <139284802+swl23@users.noreply.github.com>
2024-11-14 15:23:22 -0800

    Update script with solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    const entry = Number(x);
+    if (entry >= 1) {
+        let fibGrid = [1, 1];
+        for (i = 0; i < entry; i++) {
+            let nextFibNum = Number(fibGrid[i]) + Number(fibGrid[i+1]);
+            fibGrid.push(nextFibNum);
+            console.log(fibGrid)
+        };
+        fibMember = fibGrid[entry-1];
+        console.log(fibMember);
+        return fibMember;
+    }
+    else if (entry === 0) {
+        return 0;
+    }
+    else if (entry < 0) {
+        return 'OOPS';
+    }
 };
 
 // Do not edit below this line
