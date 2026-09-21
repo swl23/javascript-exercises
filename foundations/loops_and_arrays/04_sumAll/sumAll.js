cb05e13a65d6ffc1d2701fdb3844ed77ee81ea38
Steven Long <139284802+swl23@users.noreply.github.com>
2026-09-21 00:08:10 -0500

    fix: add missing code

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    } else if (a === b) {
+        return 0;
+    } else {
+        let big = a > b ? a : b;
+        let small = a < b ? a : b;
+        let sum = 0;
+        while (big >= small) {
+            sum += big;
+            big--;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahr) {
+    return Math.round(((fahr - 32) / (9 / 5)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cels) {
+    return Math.round((cels * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
