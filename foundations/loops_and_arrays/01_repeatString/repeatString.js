503642b6abbda84cf63d252f139a76e392a2a04e
Steven Long <139284802+swl23@users.noreply.github.com>
2026-09-20 22:21:30 -0500

    fix: add missing code

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        let result = "";
+        while (num > 0) {
+            result += string;
+            num--;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
